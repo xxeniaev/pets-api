a9da128de0295fbd3d6c84c4faf77709dfc497da
xxeniaev <xenia-ev2502@yandex.ru>
2021-06-19 18:00:02 +0500

    bugfix age

--- compose_django_project/api/models.py
+++ compose_django_project/api/models.py
@@ -14,9 +14,12 @@ class PetModel(models.Model):
     name = models.CharField(max_length=30)
     type = models.CharField(max_length=15, blank=False)
     photos = models.ManyToManyField(PhotoModel, blank=True)
-    created_at = models.DateField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     birth_date = models.DateField(blank=False, default=datetime.today().strftime('%Y-%m-%d'))
 
     @property
     def age(self):
-        return int(datetime.today().year - self.birth_date.year)
+        now = datetime.today()
+        birthday = datetime.strptime(self.birth_date.strftime('%Y-%m-%d'), '%Y-%m-%d')
+        dif = (now - birthday).days
+        return int(dif/365)
