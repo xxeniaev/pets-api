e963765cdda9ffb9eb090017b5765e412c57378d
xxeniaev <xenia-ev2502@yandex.ru>
2021-07-09 19:48:23 +0500

    refactor15

--- compose_django_project/core/settings.py
+++ compose_django_project/core/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api',
+    'compose_django_project.api',
     "rest_framework",
     "rest_framework_api_key",
 ]
